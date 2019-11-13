4dbb540ce5baf54cd0e05a5199c5d4fe11acb184
Boyke Wieberdink <Boyke@MacBook-Pro-van-MacBook.local>
2019-11-13 14:34:23 +0800

    added and connected db

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from models import db # new line
+app = Flask(__name__)
+
+@app.before_request # new line
+def before_request():
+   db.connect()
+
+@app.after_request # new line
+def after_request(response):
+   db.close()
+   return response
+
+@app.route("/")
+def index():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
